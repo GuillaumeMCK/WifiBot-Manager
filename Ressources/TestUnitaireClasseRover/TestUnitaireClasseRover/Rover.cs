ada5701813325378f026eec65252fd9df5e22cde
GuillaumeMCK <guillaume.mucwicki@gmail.com>
2022-01-06 20:33:22 +0100

    Ajout du test unitaire de la classe rover

--- Ressources/TestUnitaireClasseRover/TestUnitaireClasseRover/Rover.cs
+++ Ressources/TestUnitaireClasseRover/TestUnitaireClasseRover/Rover.cs
@@ -1,7 +1,12 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace GestionnaireWifiBot.Model
+namespace TestUnitaireClasseRover
 {
     class Rover
     {
@@ -12,12 +17,16 @@ namespace GestionnaireWifiBot.Model
         private string server_address; // Nom du serveur
         private Byte[] command;        // Tableau de 2 octets contenant les valeurs à envoyer
         private bool connected;        // Statut de la connexion
-                                       
         TcpClient clientSocket;        // Instanciation d'un client TCP qui va permettre de se connecter au serveur du robot
         NetworkStream networkStream;   // Instanciation d'un flux réseau rattaché au socket
+        Task roverTask;
 
+        //-------------
+        // Propriétés
+        //-------------
         public bool ConnectionState { get { return connected; } } // Getter du statut de la connexion au rover
-        
+        public byte[] Command { set { command = value; } } // Setter de la command a envoyer au rover
+
         //--------------
         // Constructeur
         //--------------
@@ -25,8 +34,7 @@ namespace GestionnaireWifiBot.Model
         {
             server_address = adresse;
             server_port = port_TCP;
-            command = new Byte[2] { 0x00, 0x00 };
-            clientSocket = new TcpClient();
+            command = new Byte[2] { 0xc0, 0xc0 };
             connected = false;
         }
 
@@ -39,9 +47,12 @@ namespace GestionnaireWifiBot.Model
             {
                 try
                 {
-                    clientSocket.Connect(server_address, server_port);   // Connexion au serveur avec l'adresse IP et le Port spécifié.
-                    networkStream = clientSocket.GetStream(); // Si la connexion réussi on attache le flux au canal de communication crée (socket)
+                    clientSocket = new TcpClient();                    // Creation d'un client TCP
+                    clientSocket.Connect(server_address, server_port); // Connexion au serveur avec l'adresse IP et le Port spécifié.
+                    networkStream = clientSocket.GetStream();          // Si la connexion réussi on attache le flux au canal de communication crée (socket)
                     connected = true;
+                    roverTask = new Task(() => { while (connected) SendCommand(command); });
+                    roverTask.Start();
                 }
                 catch (Exception e)
                 {
@@ -54,15 +65,14 @@ namespace GestionnaireWifiBot.Model
         //----------------------------------------------------
         // Méthode qui permet d'envoyer une commande au rover
         //----------------------------------------------------
-        public void Commander(Byte[] bytes)
+        private void SendCommand(Byte[] bytes)
         {
-            command = bytes;
-
             if (connected)
             {
                 try
                 {
                     networkStream.Write(bytes, 0, 2); // On écrit dans le flux la commande;
+                    Thread.Sleep(200);
                 }
                 catch (Exception e)
                 {
@@ -77,12 +87,20 @@ namespace GestionnaireWifiBot.Model
         //----------------------------------------------------
         public void Disconnect()
         {
+            // Arret de l'envoi de la command au rover
             if (connected)
             {
-                networkStream.Dispose();
-                clientSocket.Dispose();
+                // Fin de la tache asynchrone
                 connected = false;
+                roverTask.Wait();
+                roverTask.Dispose();
             }
+            // Liberation des ressources
+            if (networkStream != null)
+                networkStream.Close();
+            if (clientSocket != null)
+                clientSocket.Close();
         }
     }
+
 }
